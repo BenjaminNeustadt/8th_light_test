74da4a439787a6e9e8d2b1b92a37410b082fe33a
benjamin <b.james.neustadt@gmail.com>
2022-12-17 20:21:50 +0100

    Test the http connection to be true

---
+++ spec/connection_spec.rb
@@ -0,0 +1,11 @@
+#rspec test for connection
+require 'net/http'
+
+describe "API call" do
+  it "has a valid HTTP connection" do
+    API_KEY = "AIzaSyDTsdQz7h-sK8Kf3shcnXrsLT1rWj5PYak"
+    url = "https://www.googleapis.com/books/v1/volumes?q=history&key=#{API_KEY}"
+    response = Net::HTTP.get_response(URI(url))
+    expect(response.is_a?(Net::HTTPSuccess)).to be true
+  end
+end
