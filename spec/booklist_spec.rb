08f27da50768706f4548ddf3e4ada604179c2bcb
benjamin <b.james.neustadt@gmail.com>
2022-12-18 14:45:43 +0100

    Users list can hold correct data attributes
    * Right now this test is almost the same the two above it.
      There result really depends on what is added,
      so really we are testing if the correct data type can be inserted into the users list.

--- spec/booklist_spec.rb
+++ spec/booklist_spec.rb
@@ -23,9 +23,28 @@ describe "Book add to users list" do
   it "can have book data added" do
     book = BookList.new('history')
     test_data = JSON.parse(File.read('test_data.json'))
+    # Potentially use a mock here instead of using 'test_data.json'
     chosen_book = test_data["items"].first["volumeInfo"]["title"]
     book.add(chosen_book)
     expect((book.users_list).length).to eq 1
+  end
+
+  it "can have correct data added" do
+    #include the book's author, title, and publishing company
+    #list = [{ author:, title:, publishing company: }]
+    book = BookList.new('history')
+    chosen_book = {author: 'Benjamin', title:'Notes As We Go', publisher: 'Keeper of the Phones'}
+    book.add(chosen_book)
+    expect(book.users_list).to eq [{author: 'Benjamin', title:'Notes As We Go', publisher: 'Keeper of the Phones'}]
+  end
+
+  it "can have correct data added" do
+    #include the book's author, title, and publishing company
+    #list = [{ author:, title:, publishing company: }]
+    book = BookList.new('history')
+    chosen_book = {author: 'Benjamin', title:'Notes As We Go', publisher: 'Keeper of the Phones'}
+    book.add(chosen_book)
+    expect(book.users_list.first[:author]).to eq 'Benjamin'
   end
 
 end
