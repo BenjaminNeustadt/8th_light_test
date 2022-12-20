c0b77d3048f6cbdec41efa326189ad98c95c03f1
benjamin <b.james.neustadt@gmail.com>
2022-12-20 02:55:34 +0100

    Remove hardcoded api key in spec & offline option

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -3,11 +3,13 @@ require 'json'
 
 describe "API call" do
 
-  it "has a valid HTTP connection" do
-    API_KEY = "AIzaSyDTsdQz7h-sK8Kf3shcnXrsLT1rWj5PYak"
-    url = "https://www.googleapis.com/books/v1/volumes?q=history&key=#{API_KEY}"
-    response = Net::HTTP.get_response(URI(url))
-    expect(response.is_a?(Net::HTTPSuccess)).to be true
+  if $DEBUG
+    it "has a valid HTTP connection" do
+      API_KEY = JSON.parse(File.read("env.json"))["API_KEY"]
+      url = "https://www.googleapis.com/books/v1/volumes?q=history&key=#{API_KEY}"
+      response = Net::HTTP.get_response(URI(url))
+      expect(response.is_a?(Net::HTTPSuccess)).to be true
+    end
   end
 
 end
