3e78d6ff101a5939a74931b22148682d219ad41c
benjamin <b.james.neustadt@gmail.com>
2022-12-31 05:04:42 +0100

    Extract methods in BookView to module
    * Extract into another module that holds the report and formatting functionality

--- lib/bookreport.rb
+++ lib/bookreport.rb
@@ -1,5 +1,4 @@
-module Reportable
-
+module Formatter
 
 # :TODO: extract from here
   OFFLINE = true
@@ -14,6 +13,12 @@ module Reportable
     }
   }
 
+  def menu
+    MENU[:ACTION].each_with_object("") do |(option, action), menu|
+      menu << ACTION_PROMPT % {option: option, action: action}
+    end
+  end
+
   ACTION_PROMPT = "%<option>s) %<action>s\n"
 
 
@@ -27,11 +32,13 @@ module Reportable
       -----------------------
       EOS
 
-
 	LINE_STYLE = {
 		default: '=======================',
 		special: '+=+=+=+=+=+=+=+=+=+=+=+'
 	}
+
+# :TODO: methods for reporting to user
+# These should probably be extracted to a Report class
 
 	REPORT =
     <<~REPORT
@@ -41,4 +48,52 @@ module Reportable
       %<border>s
     REPORT
 
+  def report_retrieved_books
+    "AVAILABLE BOOKS:\n" << @search_results
+      .each.with_index(1)
+      .with_object("") do |(book, index), report|
+      report << list_item(book, index)
+    end
+  end
+
+  def report_books_added
+    list = []
+    @users_storage.container.each do |book|
+      list << "- #{book[:title]}"
+    end
+
+    plural = list.size == 1 ? "" : "s"
+
+    "You added %<size>i book%<plural>s:\n%<list>s" %
+      {size: list.size, plural: plural, list:list.join("\n")}
+  end
+
+	def report_booklist
+    @users_storage
+      .container.each.with_index(1)
+      .with_object(booklist = "") do |(book, index)|
+        booklist << list_item(book, index)
+      end
+
+    REPORT % {booklist: booklist, border: LINE_STYLE[:special], title: 'BOOKLIST'}
+  end
+
+  def list_item(book, index)
+    LIST_ITEM % {
+      index:     index,
+      title:     book[:title],
+      authors:   book[:authors],
+      publisher: book[:publisher]
+    }
+  end
+
+  def report_total_number_books_added
+    <<~EOS % @users_storage.container.size
+    =======================
+      Total books in user list: %i
+    =======================
+      EOS
+  end
+
 end
+
--- lib/bookview.rb
+++ lib/bookview.rb
@@ -1,74 +1,25 @@
 require_relative '../lib/booksearch'
 require_relative '../lib/bookreport'
-require 'colorize'
-
 
 class BookView
 
-include Reportable
-
-	def report_booklist
-    @users_storage
-      .container.each.with_index(1)
-      .with_object(booklist = "") do |(book, index)|
-        booklist << list_item(book, index)
-      end
-
-    REPORT % {booklist: booklist, border: LINE_STYLE[:special], title: 'BOOKLIST'}
-  end
-
-  def list_item(book, index)
-    LIST_ITEM % {
-      index:     index,
-      title:     book[:title],
-      authors:   book[:authors],
-      publisher: book[:publisher]
-    }
-  end
-
+	include Formatter
 
   def initialize
     @search_results = []
     @users_storage  = BookStorage.new
   end
 
+	public
+
   attr_accessor :users_storage, :search_results
-
-  def menu
-    MENU[:ACTION].each_with_object("") do |(option, action), menu|
-      menu << ACTION_PROMPT % {option: option, action: action}
-    end
-  end
-
-  def reset_search
-    @search_results = []
-  end
-
-  def total_books_selected
-    <<~EOS % @users_storage.container.size
-    =======================
-      Total books selected: %i
-    =======================
-      EOS
-  end
-
-  def report_books_added
-    list = []
-    @users_storage.container.each do |book|
-      list << "- #{book[:title]}"
-    end
-
-    plural = list.size == 1 ? "" : "s"
-
-    "You added %<size>i book%<plural>s:\n%<list>s" %
-      {size: list.size, plural: plural, list:list.join("\n")}
-  end
 
   def add_user_book(choice)
     @users_storage.add(@search_results[choice]) if @search_results[choice]
   end
 
   def lookup_books(search_query)
+    @search_results = []
     search = BookSearch.new(search_query, offline: OFFLINE)
     search.available_books.each.with_index(1) do |book, index|
       list_item(book, index)
@@ -76,13 +27,9 @@ include Reportable
     end
   end
 
-  def report_retrieved_books
-    "AVAILABLE BOOKS:\n" << @search_results
-      .each.with_index(1)
-      .with_object("") do |(book, index), report|
-      report << list_item(book, index)
-    end
+  # :TODO: this should actually be inside the lookup_books method, it reset the search
+  def reset_search
+    @search_results = []
   end
-
-
 end
+
