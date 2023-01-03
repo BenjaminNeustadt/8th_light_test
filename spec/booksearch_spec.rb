57b17e12bd505e36255019cf0b49db66028db7c4
benjamin <b.james.neustadt@gmail.com>
2023-01-03 18:52:48 +0100

    Move tests to BookSearch from e2e
    * We can move the tests that actually belong inside BookSearch, and that
      are not yet testing end to end usage of program

--- spec/booksearch_spec.rb
+++ spec/booksearch_spec.rb
@@ -4,7 +4,8 @@ require_relative '../lib/strategy/bookdata'
 RSpec.describe BookSearch do
 
   before :each do
-    @test_book_data = JSON.parse(File.read('spec/test_data.json'))
+    @test_book_data = TestData.new.parse
+    # @test_book_data = JSON.parse(File.read('spec/test_data.json'))
     @isolated_list = BookSearch.new('history', @test_book_data)
   end
 
@@ -48,14 +49,63 @@ RSpec.describe BookSearch do
 
   end
 
+    context 'available books' do
+      it "returns 5 books from the search for a given word" do
+        booksearch = BookSearch.new("tech", @test_book_data)
+        expect((booksearch.available_books).length).to eq 5
+      end
+    end
+
   context "storage" do
 
-    it 'can hold data via BookStorage' do
+    it " can hold data" do
+      booksearch = BookSearch.new("tech", @test_book_data)
+      retrieved_data = booksearch.available_books
+      # We choose to add the first book
+      chosen_book = retrieved_data[0]
+      booksearch.storage.add(chosen_book)
+
+      expect(booksearch.storage).to be_truthy
+      expect((booksearch.storage.container).length).to eq 1
+    end
+
+    it 'can have a book added via BookStorage' do
       list = @isolated_list
       chosen_book = {author: "Benjamin", title: "Notes As We Go", publisher: "Keeper of the Phones"}
       list.storage.add(chosen_book)
       expect(list.storage.container)
         .to eq [{author: "Benjamin", title: "Notes As We Go", publisher: "Keeper of the Phones"}]
+    end
+
+    it "can have a book added after one has already been added" do
+      booksearch = BookSearch.new("tech", @test_book_data)
+      retrieved_books = booksearch.available_books
+      # We choose to add the first book
+      booksearch.storage.add(retrieved_books.first)
+
+      actual = (booksearch.storage.container).length
+      expected = 1
+      expect(actual).to eq expected
+
+      # We choose to add another book
+      booksearch.storage.add(retrieved_books.last)
+      actual = (booksearch.storage.container).length
+      expected = 2
+      expect(actual).to eq expected
+    end
+
+    it "stores authors, title, and publisher" do
+      booksearch = BookSearch.new("tech", @test_book_data)
+      retrieved_books = booksearch.available_books
+      booksearch.storage.add(retrieved_books.first)
+      actual = (booksearch.storage.container)
+      expect(actual).to be_an Array
+
+      actual = actual.first
+      expect(actual).to be_a Hash
+      expect(actual).to have_key :authors
+      expect(actual).to have_key :publisher
+      expect(actual).to have_key :title
     end
 
   end
