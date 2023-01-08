28200e0fb1c69a6c899a65d1928ade3df2630c96
benjamin <b.james.neustadt@gmail.com>
2023-01-08 18:04:11 +0100

    Modify argument name in method

--- lib/bookview.rb
+++ lib/bookview.rb
@@ -24,9 +24,9 @@ class BookView
     @users_storage.add(@search_results[choice]) if @search_results[choice]
   end
 
-  def lookup_books(choice)
+  def lookup_books(query)
     @search_results = []
-    search = BookSearch.new(SOURCE[choice])
+    search = BookSearch.new(SOURCE[query])
     search.available_books.each.with_index(1) do |book, index|
       item(book, index)
       @search_results << book
