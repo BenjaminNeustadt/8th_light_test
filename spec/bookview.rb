667766dea1ef80bafb960e3e70aea8165701f15f
benjamin <b.james.neustadt@gmail.com>
2023-01-16 22:08:47 +0100

    WIP: spec for BookView instantiate

---
+++ spec/bookview.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/bookview'
+
+RSpec.describe BookView do
+
+  context 'initialize' do
+
+    it 'instantiates an instance of BookStorage' do
+      bookview = BookView.new
+      expect(bookview.storage).to eq
+    end
+
+  end
+
+end
